d7c39e75ea27145a26db4b5e83424d027b9e9f7d
Andre lumertz Martins <andre.martins@hmv.org.br>
2021-05-10 10:50:07 -0300

    mudança no repeticao.php

--- repeticao.php
+++ repeticao.php
@@ -1,14 +1,23 @@
 <?php   
- //  repetição
+
+
+
+// break and continue 
 
 $contador= 1;
 
-
-while ($contador <= 15) {
-  echo "$contador" . PHP_EOL;
-  $contador = $contador + 1;
+for ($contador= 1; $contador <= 15; $contador++) {
+    if ($contador == 13 ) {
+     echo  "Esse é o numero 13" . PHP_EOL;
+    }
+    else {
+      echo "$contador" . PHP_EOL;
+    }
 };
 
-// end repetição 
 
 
+
+
+
+// end break and continue 
